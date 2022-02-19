7365bed966c1ca05de8ee9b65404e5c640e4e090
yaromir-zhuravsky <yaromir.zhuravsky@gmail.com>
2022-02-19 15:07:04 +0300

    Finish color flip project

---
+++ 01_color_flipper/app.js
@@ -0,0 +1,18 @@
+const colors = ["green", "red", "rgba(133,122,200)", "#f15025"];
+const btn = document.getElementById('btn');
+const color = document.querySelector('.color');
+
+function getRandomInt(max) {
+  return Math.floor(Math.random() * max);
+}
+
+btn.addEventListener('click', function(){
+    // get random number between 0 - 3
+     //const randomNumber = rgb(getRandomInt(256), getRandomInt(256), getRandomInt(256));
+     const red =  getRandomInt(256).toString(16);
+     const blue = getRandomInt(256).toString(16);
+     const green = getRandomInt(256).toString(16);
+     const hex = '#' + red + green + blue
+     document.body.style.backgroundColor = hex
+     color.innerHTML = hex
+})
